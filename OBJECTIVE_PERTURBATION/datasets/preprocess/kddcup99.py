7d004f295ecb6c08ae77be99226f6a9ed3e224f3
Fzshuo <113944584+fang59@users.noreply.github.com>
2025-07-17 16:18:53 -0500

    Add files via upload

--- OBJECTIVE_PERTURBATION/datasets/preprocess/kddcup99.py
+++ OBJECTIVE_PERTURBATION/datasets/preprocess/kddcup99.py
@@ -1,78 +1,3 @@
-# import os
-# import numpy as np
-# import sklearn.datasets
-# from sklearn import preprocessing
-# from utils.utils_preprocessing import convert_to_binary, normalize_rows, format_output
-#
-#
-#
-# FILENAME_X = 'kddcup99_processed_x.npy'
-# FILENAME_Y = 'kddcup99_processed_y.npy'
-#
-#
-# """
-# Preprocess the kddcup99 dataset
-#
-# This process is discussed in section 7.1 of CMS11:
-# \"For the KDDCup99 data set, the instances were preprocessed by converting
-# each categorial attribute to a binary vector. Each column was normalized
-# to ensure that the maximum value is 1, and finally, each row was normalized,
-# to ensure that the norm of any example is at most 1. After preprocessing,
-# each example was represented by a 119-dimensional vector, of norm at most 1.\"
-#
-# My code to preprocess the categorial features was somewhat based upon:
-# https://biggyani.blogspot.com/2014/08/using-onehot-with-categorical.html
-#
-# The preprecessed data gets saved to a file hardcoded into this script.
-# """
-#
-#
-# def preprocess(cache_location, output_location):
-#
-#     np.random.seed(10000019)
-#     os.environ['SCIKIT_LEARN_DATA'] = cache_location
-#
-#     subset = sklearn.datasets.fetch_kddcup99(percent10=True)
-#
-#     # Randomly select 70,000 elements
-#     # Based on https://stackoverflow.com/a/14262743/859277
-#
-#     indices = np.random.randint(subset['data'].shape[0], size=70000)
-#
-#     subset['data'] = subset['data'][indices, :]
-#     subset['target'] = subset['target'][indices]
-#
-#     symbolic_cols = []
-#     continuous_cols = []
-#     le = preprocessing.LabelEncoder()
-#
-#     for i in range(subset['data'].shape[1]):
-#         col = subset['data'][:, i]
-#
-#         if type(col[0]) == bytes:
-#             numeric = le.fit_transform(col)
-#             symbolic_cols.append(numeric)
-#         else:
-#             continuous_cols.append(col)
-#
-#     symbolic_cols = convert_to_binary(symbolic_cols)
-#
-#     combined_data = np.column_stack(symbolic_cols + continuous_cols)
-#     final_data = combined_data
-#
-#     all_data = np.column_stack([final_data, format_output(subset['target'])])
-#     np.random.shuffle(all_data)
-#
-#
-#     print(all_data[:, -1])
-#     print(len(all_data))
-#     print(len(all_data[0]))
-#     np.save(os.path.join(output_location, FILENAME_X), all_data[:, :-1])
-#     np.save(os.path.join(output_location, FILENAME_Y), all_data[:, -1])
-#
-#
-
-
 import os
 import numpy as np
 import sklearn.datasets
@@ -86,9 +11,10 @@ def preprocess(cache_location, output_location):
     np.random.seed(10000019)
     os.environ['SCIKIT_LEARN_DATA'] = cache_location
 
+    # Load 10% subset of KDDCup99 dataset
     subset = sklearn.datasets.fetch_kddcup99(percent10=True)
 
-    # 随机选择 70,000 个样本
+    # Randomly sample 70,000 examples
     indices = np.random.randint(subset['data'].shape[0], size=70000)
     subset['data'] = subset['data'][indices, :]
     subset['target'] = subset['target'][indices]
@@ -100,21 +26,24 @@ def preprocess(cache_location, output_location):
     for i in range(subset['data'].shape[1]):
         col = subset['data'][:, i]
 
-        if isinstance(col[0], bytes):  # 处理类别型数据
-            numeric = le.fit_transform(col).astype(np.float32)  # 转换为数值型
+        if isinstance(col[0], bytes):
+            # Process categorical feature by encoding to numeric values
+            numeric = le.fit_transform(col).astype(np.float32)
             symbolic_cols.append(numeric)
-        else:  # 处理连续型数据
-            continuous_cols.append(col.astype(np.float32))  # 转换为 float32
+        else:
+            # Process continuous feature
+            continuous_cols.append(col.astype(np.float32))
 
-    symbolic_cols = convert_to_binary(symbolic_cols)  # One-hot 编码
+    # One-hot encode the categorical features
+    symbolic_cols = convert_to_binary(symbolic_cols)
 
-    # 合并数值型和类别型特征
+    # Concatenate categorical and continuous features
     combined_data = np.column_stack(symbolic_cols + continuous_cols).astype(np.float32)
 
-    # 处理标签数据
+    # Format and encode the labels
     labels = format_output(subset['target']).astype(np.float32)
 
-    # 组合所有数据
+    # Combine features and labels, then shuffle the full dataset
     all_data = np.column_stack([combined_data, labels])
     np.random.shuffle(all_data)
 
@@ -122,7 +51,6 @@ def preprocess(cache_location, output_location):
     print(len(all_data))
     print(len(all_data[0]))
 
-    # **转换数据格式后再保存**
+    # Save processed feature and label arrays
     np.save(os.path.join(output_location, FILENAME_X), all_data[:, :-1].astype(np.float32))
     np.save(os.path.join(output_location, FILENAME_Y), all_data[:, -1].astype(np.float32))
-
--- OBJECTIVE_PERTURBATION/datasets/preprocess/mnist.py
+++ OBJECTIVE_PERTURBATION/datasets/preprocess/mnist.py
@@ -7,38 +7,42 @@ from utils.utils_preprocessing import convert_to_binary, normalize_rows, format_
 FILENAME_X = 'mnist_processed_x.npy'
 FILENAME_Y = 'mnist_processed_y.npy'
 
-
 def preprocess(cache_location, output_location):
-
     np.random.seed(10000019)
 
-
+    # Define transformation to convert images to tensors
     transform = transforms.Compose([transforms.ToTensor()])
 
+    # Load training and test datasets
     mnist_train = datasets.MNIST(root=os.path.join(cache_location, "MNIST_data"),
                                  train=True, download=True, transform=transform)
 
     mnist_test = datasets.MNIST(root=os.path.join(cache_location, "MNIST_data"),
                                 train=False, download=True, transform=transform)
 
+    # Flatten the image tensors and extract labels
     train_features = np.array([np.array(img[0]).flatten() for img in mnist_train])
     train_labels = np.array([img[1] for img in mnist_train])
 
     test_features = np.array([np.array(img[0]).flatten() for img in mnist_test])
     test_labels = np.array([img[1] for img in mnist_test])
 
+    # One-hot encode the labels
     train_labels = np.eye(10)[train_labels]
     test_labels = np.eye(10)[test_labels]
 
+    # Combine training and testing data
     features_set = np.vstack((train_features, test_features))
     labels_set = np.vstack((train_labels, test_labels))
 
     label_width = labels_set.shape[1]
 
+    # Combine features and labels into one array
     combined_data = np.column_stack([features_set, labels_set])
 
+    # Shuffle the combined dataset
     np.random.shuffle(combined_data)
 
-    np.save(os.path.join(output_location, FILENAME_X), combined_data[:, :-label_width])  # 只存图像数据
-    np.save(os.path.join(output_location, FILENAME_Y), combined_data[:, -label_width:])  # 只存标签
-
+    # Save processed features and labels separately
+    np.save(os.path.join(output_location, FILENAME_X), combined_data[:, :-label_width])  # Save only image data
+    np.save(os.path.join(output_location, FILENAME_Y), combined_data[:, -label_width:])  # Save only label data
